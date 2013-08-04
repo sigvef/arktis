5b070b39adb0edbf3745f5915b65656e0b68a7ca
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-08-04 22:34:11 +0200

    Evolved the music player

--- music.php
+++ music.php
@@ -5,8 +5,22 @@
 
 <style>
 
+
+* {
+    -webkit-transition: all .3s ease;
+    -moz-transition: all .3s ease;
+    transition: all .3s ease;
+}
+
+.description{
+    padding: 0;
+    font-size: 0.8em;
+    line-height: 1.2em;
+}
+
 .song{
-    padding: 20px;
+    cursor: pointer;
+    padding: 0;
 }
 
 .song .by{
@@ -19,10 +33,25 @@
 
 .song .description{
     display: none;
+    padding: 20px;
+}
+
+.song .name{
+    padding-left: 10px;
 }
 
 .song.playing{
     background: #ccc;
+}
+
+.playlist {
+    border: 1px solid #ccc;
+    background: #eee;
+    margin-bottom: 20px;
+}
+
+.playlist > * {
+    padding: 10px 20px; 
 }
 
 .playlist.playing{
@@ -36,39 +65,54 @@
 </head>
 <body>
 
+<h1>Music</h1>
+
+<p>
+This is a collection of music I have made.
+Some is recent, some is quite old. 
+Click a track to play it.
+</p>
+
 <div id=demoscene class=playlist>
-    <h2>Demoscene songs</h2>
+    <h3>Demoscene songs</h3>
+    <div class=description>Some tracks from my participation in the demoscene.</div>
 
     <div class=song>
     <span class=name>TUNL-MNTN-WTER</span>
     <span class=by>Ninjadev</span>
-    <div class=description>Bla bla yoabout this track</div>
-    <audio src=https://github.com/sigvef/skog/releases/download/v1.0/music.mp3>
+    <div class=description>This is the soundtrack for the Ninjadev demo called TUNL-MNTN-WTER.</div>
+    <audio src=tunl-mntn-wter.mp3>
     </div>
 
     <div class=song>
     <span class=name>HONEYCOMB</span>
     <span class=by>Ninjadev</span>
-    <div class=description>Bla bla bla about this track</div>
-    <audio src=http://jord.al/honeycomb/music.ogg>
+    <div class=description>This is the soundtrack for the Ninjadev demo called HONEYCOMB by Ninjadev.</div>
+    <audio src=honeycomb.mp3>
     </div>
 </div>
 
-<div id=trondheim-guitar-sextet class=playlist>
-    <h2>Trondheim Guitar Sextet</h2>
+<div id=random class=playlist>
+    <h3>Random</h3>
+    <div class=description>Just some songs.</div>
 
     <div class=song>
-    <span class=name>TUNL-MNTN-WTER</span>
-    <span class=by>Ninjadev</span>
-    <div class=description>Bla bla yoabout this track</div>
-    <audio src=https://github.com/sigvef/skog/releases/download/v1.0/music.mp3>
+    <span class=name>Unbreakable</span>
+    <span class=by>sigveseb</span>
+    <div class=description></div>
+    <audio src=unbreakable.mp3>
     </div>
+</div>
+
+<div id=remixes class=playlist>
+    <h3>Remixes</h3>
+    <div class=description>Some remixes that I have made.</div>
 
     <div class=song>
-    <span class=name>HONEYCOMB</span>
-    <span class=by>Ninjadev</span>
-    <div class=description>Bla bla bla about this track</div>
-    <audio src=http://jord.al/honeycomb/music.ogg>
+    <span class=name>Doin' It Right feat. Giorgio Moroder</span>
+    <span class=by>sigveseb</span>
+    <div class=description>I've been wanting to make a remix of Daft Punk's "Doin' It Right" for a long time, and this is the result. The Giorgio quotes are taken from the REM Collaborators interview.</div>
+    <audio src=doin-it-right-feat-giorigo-moroder.mp3>
     </div>
 </div>
 
