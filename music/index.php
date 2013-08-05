32ccc2782359443df8db8c7949685303f62d3058
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-08-05 02:05:15 +0200

    Styling, css to own file

--- music/index.php
+++ music/index.php
@@ -2,88 +2,9 @@
 <head>
 <title>Music :: Arktis by Sigve Sebastian Farstad</title>
 <link rel="stylesheet" href="/css.css">
+<link rel="stylesheet" href="music.css">
 <script src="Player.js"></script>
 
-<style>
-
-
-* {
-    -webkit-transition: all .3s ease;
-    -moz-transition: all .3s ease;
-    transition: all .3s ease;
-}
-
-.description{
-    padding: 0;
-    font-size: 0.8em;
-    line-height: 1.2em;
-}
-
-.progressbar{
-    height: 5px;
-    width: 0;
-    background: #222;
-    -webkit-transition: width 1s linear;
-    -moz-transition: width 1s linear;
-    transition: width 1s linear;
-}
-
-.progressbar-container{
-    display:none;
-    width: 580px;
-    margin-bottom: -10px;
-    margin-left: -20px;
-    margin-right: -20px;
-}
-
-.song.playing .progressbar-container{
-    display: block;
-}
-
-.song{
-    cursor: pointer;
-    padding: 0;
-}
-
-.song .by{
-    opacity: 0.5;
-}
-
-.song .by::before{
-    content: "by ";
-}
-
-.song .description{
-    display: none;
-    padding: 20px;
-}
-
-.song .name{
-    padding-left: 10px;
-}
-
-.song.playing{
-    background: #ccc;
-}
-
-.playlist {
-    border: 1px solid #ccc;
-    background: #eee;
-    margin-bottom: 20px;
-}
-
-.playlist > * {
-    padding: 10px 20px; 
-}
-
-.playlist.playing{
-}
-
-.song.playing .description{
-    display: block;
-}
-
-</style>
 </head>
 <body>
 
@@ -95,10 +16,10 @@ Some is recent, some is quite old.
 Click a track to play it.
 </p>
 
-<div id=demoscene class=playlist>
-    <h3>Demoscene songs</h3>
-    <div class=description>Some tracks from my participation in the demoscene.</div>
+<h3>Demoscene songs</h3>
+<div class=description>Some tracks from my participation in the demoscene.</div>
 
+<div id=demoscene class=playlist>
     <div class=song>
     <span class=name>TUNL-MNTN-WTER</span>
     <span class=by>Ninjadev</span>
@@ -120,10 +41,9 @@ Click a track to play it.
     </div>
 </div>
 
+<h3>Random</h3>
+<div class=description>Just some songs.</div>
 <div id=random class=playlist>
-    <h3>Random</h3>
-    <div class=description>Just some songs.</div>
-
     <div class=song>
     <span class=name>Unbreakable</span>
     <span class=by>sigveseb</span>
@@ -135,10 +55,9 @@ Click a track to play it.
     </div>
 </div>
 
+<h3>Remixes</h3>
+<div class=description>Some remixes that I have made.</div>
 <div id=remixes class=playlist>
-    <h3>Remixes</h3>
-    <div class=description>Some remixes that I have made.</div>
-
     <div class=song>
     <span class=name>Doin' It Right feat. Giorgio Moroder</span>
     <span class=by>sigveseb</span>
