6fad894a957f927514f11ec1f48de363f04a9209
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-08-19 16:05:06 +0200

    Added fff page

---
+++ ffff/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Foreningen For Fonetiske Forkortelser :: Arktis by Sigve Sebastian Farstad</title>
+<link rel="stylesheet" href="/css.css">
+<meta charset=utf-8>
+<? require('../stats.inc');?>
+</head>
+<body>
+
+<header>
+<nav>
+<a href="/">Arktis</a>
+ » 
+<a href="/ffff">Foreningen For Fonetiske Forkortelser</a>
+</nav>
+<h1>Foreningen For Fonetiske Forkortelser</h1>
+</header>
+
+<div id=content>
+
+<p>Foreningen for Fonetiske Forkortelser frammer forståelse for fanesaken: "Fonetisk fattelige forkortelser for folket", fordi foreningen føler folk flest faller farlig fort for fristelsen "forkortelsesfjerning" før forkortelser forsøkes framført. Foreningen finner forslag for forbedring faglig forfriskende, frakt fonetiske forkortelsesforslag fakturafritt fram foran forslagskassa: ffff@arkt.is.</p>
+<p>
+Følgende forkortelsesliste foreligger for foreningens fagmedlemmer:
+</p>
+
+<dl>
+<dt>BRB<br />(Be Right Back)</dt>
+<dd>/'brīb/</dd>
+<dt>C++<br /></dt>
+<dd>/'sep/</dd>
+<dt>FFFF<br />(Foreningen for Fonetiske Forkortelser)</dt>
+<dd>/ˈfē ˈfä ˈfu ˈfüm/</dd>
+<dt>IRC<br />(Internet Relay Chat)</dt>
+<dd>/iˋrɑ:k/</dd>
+<dt>.js<br />(JavaScript extension)</dt>
+<dd>/jɑs/</dd>
+<dt>KTN<br />(Kommunikasjon, tjenester og nett)</dt>
+<dd>/ˈki-t<sup>ə</sup>n/</dd>
+<dt>MMI<br />(Menneske-maskin-interaksjon)</dt>
+<dd>/'mü-mē/</dd>
+<dt>.PDF<br />(Portable Document Format)</dt>
+<dd>/ˈpe-də-ˌfī(-ə)l, ˈpē-/</dd>
+<dt>PHP<br />(PHP Hypertext Preprocessor)</dt>
+<dd>/ˈfap/</dd>
+<dt>.PNG<br />(Portable Network Graphics)</dt>
+<dd>/ˈpiŋ/</dd>
+<dt>SSH<br />(Secure SHell)</dt>
+<dd>/ˈsash/</dd>
+<dt>SQL<br />(Structured Query Language)</dt>
+<dd>/ˈskwēl/</dd>
+<dt>.SVG<br />(Scalable Vector Graphics)</dt>
+<dd>/ˈswag/</dd>
+</dl>
+
+</div>
+<? require('../footer.inc');?>
+
+</body>
+</html>
