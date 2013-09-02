a824fdab9b055953b4f2325892bac7690c6a095c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-09-02 15:08:57 +0200

    Re-add E-V-R-Y lyrics

---
+++ evry/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>E-V-R-Y :: Arktis by Sigve Sebastian Farstad</title>
+<link rel="stylesheet" href="/css.css">
+<meta charset=utf-8>
+<? require('../stats.inc');?>
+</head>
+<body>
+
+<header>
+<nav>
+<a href="/">Arktis</a>
+ » 
+<a href="/evry">E-V-R-Y</a>
+</nav>
+<h1>E-V-R-Y</h1>
+</header>
+
+<div id=content>
+
+<p>
+Not too long ago,<br />
+Thousands of people joined forces,<br />
+Sharing the same vision,<br />
+They hammered out a clear mission.<br />
+Tonight,<br />
+Representing EVRY,<br />
+Only one challenge remains,<br />
+And that is:<br />
+To get the party started!<br />
+<br />
+So come on everybody,<br />
+Be strong and,<br />
+Embrace the infinite opportunities!<br />
+<br />
+We got the power.<br />
+We got the power.<br />
+We got the power.<br />
+<br />
+Mike check one two one two,<br />
+Check this out.<br />
+<br />
+Who can you call to get the job done, <em>(who)</em><br />
+EVRY is still number one. <em>(yeah)</em><br />
+We embrace opportunity,<br />
+Bring information to life,<br />
+You and me, tell me:<br />
+Who's the king?<br />
+Process is king!<br />
+Who's the king?<br />
+Product is king!<br />
+Who's the king?<br />
+Customer is king!<br />
+Who's the king?<br />
+EVRY is king!<br />
+<br />
+We got the power.<br />
+We got the power.<br />
+<br />
+I'm the CEO,<br />
+turn my mike up,<br />
+Turn it up.<br />
+<br />
+Kings of the throne forever, <em>(yeah!)</em><br />
+As a team we perform together, <em>(yeah!)</em><br />
+Empowered inspired beyond any expectations.<br />
+Question, tell me:<br />
+Who's the king?<br />
+Process is king!<br />
+Who's the king?<br />
+Product is king!<br />
+Who's the king?<br />
+Customer is king!<br />
+Who's the king?<br />
+EVRY is king!<br />
+<br />
+They say we're number one,<br />
+On the throne,<br />
+I've heard that's what they say.<br />
+They say it's EVRY in the house,<br />
+People know that we don't play.<br />
+We anticipate needs,<br />
+All on point,<br />
+They know we never miss.<br />
+Evry has a song,<br />
+In the stash,<br />
+And it goes a little something like this: <em>(this)</em><br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+'Cause we are the queens!<br />
+<br />
+Do it again.<br />
+Come on.<br />
+Once again.<br />
+Turn it up.<br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+<em>(just give us a)</em><br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+E-V-R-Y!<br />
+<br />
+'Cause we are the queens!<br />
+<br />
+<em style="float:none;">*scratch*</em> EVRY!<br />
+2012, CEO,<br />
+Check this out.<br />
+<br />
+We got the power.
+</p>
+
+</div>
+
+<? require('../footer.inc');?>
+
+</body>
+</html>
