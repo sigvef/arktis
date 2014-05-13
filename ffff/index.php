1b4e7047287dd84f6a0df97a0417d5de53f8e627
iver56 <iver56@hotmail.com>
2014-05-13 16:52:53 +0200

    DO/dosh

--- ffff/index.php
+++ ffff/index.php
@@ -54,8 +54,8 @@ Følgende forkortelsesliste foreligger for foreningens fagmedlemmer:
 <dd>/ˈswag/</dd>
 <dt>.DMG<br />(Apple Disk Image File)</dt>
 <dd>/ˈdæmɪdʒ/</dd>
-<dt>DigO<br />(Digital Ocean)</dt>
-<dd>/dɪɡo/</dd>
+<dt>DO<br />(Digital Ocean)</dt>
+<dd>/ˈdō-sh/</dd>
 </dl>
 
 </div>
