7e67ef63c02e4dd6568346ac4fc3afa1929616c3
iver56 <iver56@hotmail.com>
2014-07-04 19:49:14 +0200

    ffff: FLP

--- ffff/index.php
+++ ffff/index.php
@@ -32,6 +32,8 @@ Følgende forkortelsesliste foreligger for foreningens fagmedlemmer:
 <dd>/'sep/</dd>
 <dt>FFFF<br />(Foreningen for Fonetiske Forkortelser)</dt>
 <dd>/ˈfē ˈfä ˈfu ˈfüm/</dd>
+<dt>FLP<br />(FL Studio Project)</dt>
+<dd>/flip/</dd>
 <dt>IRC<br />(Internet Relay Chat)</dt>
 <dd>/iˋrɑ:k/</dd>
 <dt>.js<br />(JavaScript extension)</dt>
